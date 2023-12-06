4e9c43197a5fc6b8ab06ca73ec7dd9bdc11b6c65
Alexander Kadantsev <xarawg@gmail.com>
2023-12-06 18:47:30 +0500

    sanitize dto id row

--- backend/src/SecurityService_Core/Models/DTO/UserDTO.cs
+++ backend/src/SecurityService_Core/Models/DTO/UserDTO.cs
@@ -2,7 +2,6 @@
 {
     public class UserDTO
     {
-        public Guid IdUser { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
